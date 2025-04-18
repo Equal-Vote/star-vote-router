76304eddb24a3537e8fc39297a453cf1452ab72e
Arend Peter <apc1993@gmail.com>
2025-04-17 22:04:51 -0700

    Update the redirect logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, HashRouter} from 'react-router-dom'
+import { useParams } from "react-router";
+
 import Prompt from './Prompt'
-import SlugRedirect from './SlugRedirect'
 
 export const targetDomains = {
     old: 'https://classic.star.vote',
@@ -11,10 +12,11 @@ export const autoRedirectDelay = 0; // we'll set this to 0 later
 // to reset the autoredirect clear the browsing data for star.vote
 // https://superuser.com/questions/1787991/clear-browsing-history-from-specific-site-on-chrome
 
-export default () => <HashRouter> {/* https://www.freecodecamp.org/news/deploy-a-react-app-to-github-pages/ */}
-    <Routes>
-        <Route path='/' element={<Prompt/>}/>
-        {/* this slug currently isn't used since github pages doesn't support it, instead 404.html is hacked to account for this */ }
-        <Route path='/:slug' element={<SlugRedirect/>}/>
-    </Routes>
-</HashRouter>
+export default () => {
+    let parts = window.location.href.split(/star.vote|localhost:3000/);
+    if(parts.length >= 2){
+        window.location.href = `https://classic.star.vote/${parts[1].replace('/','')}`;
+        return <></>
+    }
+    return <Prompt/>
+}
